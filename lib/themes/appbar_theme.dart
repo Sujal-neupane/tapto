ec578e099eaf98143a9d1a2a9478b20d7ca2e46d
sujal-sudo <np.sujal2004@gmail.com>
2025-12-10 12:19:01 +0545

    made the theme folder with appbar theme, bottom navigation theme and also text editing theme and also made some changes into app.dart

---
+++ lib/themes/appbar_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+AppBarTheme extendedAppBarTheme = AppBarTheme(
+  backgroundColor: Colors.white,
+  titleTextStyle: TextStyle(
+    color: Colors.black,
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  ),
+  iconTheme: IconThemeData(color: Colors.black),
+);
