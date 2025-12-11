648024105eafa515eb72e701620ad5be0420b9ee
sujal-sudo <np.sujal2004@gmail.com>
2025-12-11 10:13:35 +0545

    include the font folder and added the main font for the application and made changes in pubspec yaml

--- lib/theme/components/icon_theme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import '../colors.dart';
-
-class AppIconTheme {
-  static const IconThemeData light = IconThemeData(
-    color: AppColors.textPrimary,
-    size: 24,
-  );
-}
