fdd946c9402cecf9afc74b230a38ab92287b9073
sujal-sudo <np.sujal2004@gmail.com>
2025-12-17 22:14:54 +0545

    made changes in the app file for the routing purpose of the application

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:tapto/screens/auth/forgot_password_screen.dart';
 import 'package:tapto/screens/auth/otp_screen.dart';
-// import 'package:tapto/screens/wish_list_screen.dart';
+import 'package:tapto/screens/cart_screen.dart';
 import 'package:tapto/screens/dashboard_screen.dart';
+import 'package:tapto/screens/filter_screen.dart';
 import 'package:tapto/screens/onboarding_screen.dart';
+import 'package:tapto/screens/search_screen.dart';
 import 'package:tapto/screens/signp_screen.dart';
 import 'package:tapto/screens/splash_screen.dart';
 import 'package:tapto/screens/login_screen.dart';
+import 'package:tapto/screens/wish_list_screen.dart';
 import 'package:tapto/theme/app_theme.dart';
 
 class MyApp extends StatelessWidget {
@@ -26,6 +29,10 @@ class MyApp extends StatelessWidget {
         '/onboarding': (context) => const OnboardingScreen(),
         '/forgot-password': (context) => const ForgotPasswordScreen(),
         '/otp': (context) => const OtpScreen(),
+        '/filter': (context) => const FilterScreen(),
+        '/search': (context) => const SearchScreen(),
+        '/cart': (context) => const CartScreen(),
+        '/wish-list': (context) => const WishlistScreen(),
       },
     );
   }
